6b38cb0fc4caf96184a2d86c8107bb72014e44eb
Ahaochan <844394093@qq.com>
2018-05-08 16:36:45 +0800

    AC

---
+++ src/LintCode/_145大小写转换.java
@@ -0,0 +1,24 @@
+package LintCode;
+
+import java.util.Scanner;
+
+/**
+ * 145. 大小写转换
+ * 描述
+ * 将一个字符由小写字母转换为大写字母
+ * 样例
+ * a -> A
+ * b -> B
+ */
+public class _145大小写转换 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char c = in.next().charAt(0);
+        System.out.println(lowercaseToUppercase(c));
+    }
+
+    public static char lowercaseToUppercase(char character) {
+        return (char) (character - 32);
+    }
+}
