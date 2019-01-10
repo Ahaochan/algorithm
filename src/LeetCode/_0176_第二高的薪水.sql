5e49a08d00827acaa72e890b36828bdd60098cc5
Ahaochan <844394093@qq.com>
2019-01-11 00:39:56 +0800

    添加地址

--- src/LeetCode/_0176_第二高的薪水.sql
+++ src/LeetCode/_0176_第二高的薪水.sql
@@ -1,3 +1,5 @@
+# https://leetcode-cn.com/problems/second-highest-salary/
+
 # 编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
 
 # +----+--------+
--- src/LeetCode/_0177_第N高的薪水.sql
+++ src/LeetCode/_0177_第N高的薪水.sql
@@ -1,3 +1,5 @@
+# https://leetcode-cn.com/problems/nth-highest-salary/
+
 # 编写一个 SQL 查询，获取 Employee 表中第 n 高的薪水（Salary）。
 #
 # +----+--------+
