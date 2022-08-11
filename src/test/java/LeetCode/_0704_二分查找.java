6fa872b4a68c9af469b7e0aa021805229814771c
Ahaochan <844394093@qq.com>
2022-08-11 10:54:29 +0800

    _0704_二分查找

--- src/test/java/LeetCode/_0704_二分查找.java
+++ src/test/java/LeetCode/_0704_二分查找.java
@@ -4,40 +4,71 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * https://leetcode-cn.com/problems/binary-search/
- * 题目描述:
- * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
- * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
- *
- * 解题思路
- * 二分查找
+ * 题目: https://leetcode-cn.com/problems/binary-search/
+ * 题解: https://labuladong.github.io/algo/1/11/
  */
 public class _0704_二分查找 {
 
     @Test
-    public void test1() {
+    public void test() {
         Assert.assertEquals(4, search(new int[]{-1, 0, 3, 5, 9, 12}, 9));
-        Assert.assertEquals(-1, search(new int[]{-1, 0, 3, 5, 9, 12},2));
+        Assert.assertEquals(-1, search(new int[]{-1, 0, 3, 5, 9, 12}, 2));
         Assert.assertEquals(-1, search(new int[]{-1, 0, 3, 5, 9, 12}, 13));
+        Assert.assertEquals(5, search(new int[]{-1, 0, 3, 5, 9, 12}, 12));
+        Assert.assertEquals(0, search(new int[]{5}, 5));
     }
 
-    /**
-     * 执行用时 : 5 ms, 在Binary Search的Java提交中击败了34.67% 的用户
-     * 内存消耗 : 48.8 MB, 在Binary Search的Java提交中击败了0.77% 的用户
-     */
     public int search(int[] nums, int target) {
-        int l = 0, r = nums.length - 1;
-        int mid;
-        while (l < r) {
-            mid = l + (r - l) / 2;
-            if(nums[mid] < target) {
-                l = mid + 1;
-            } else if(nums[mid] > target) {
-                r = mid - 1;
-            } else {
+        return searchV4(nums, target);
+    }
+
+    public int searchV2(int[] nums, int target) {
+        int l = 0, r = nums.length - 1; // [l, r]中查找
+        while (l <= r) { // l == r + 1 跳出, 也就是[r+1, r]区间
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
                 return mid;
+            // 因为nums[mid]已经判断过了, 所以要去[left, mid-1] 或 [mid + 1, right]
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int searchV3(int[] nums, int target) {
+        int l = 0, r = nums.length - 1; // [l, r]中查找
+        while (l < r) { // l == r 跳出, 也就是[r, r]区间, 要打补丁判断nums[l] == target
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            // 因为nums[mid]已经判断过了, 所以要去[left, mid-1] 或 [mid + 1, right]
+            } else if (nums[mid] < target) {
+                // 因为nums[mid]已经判断过了, 所以+1
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                // 因为nums[mid]已经判断过了, 所以-1
+                r = mid - 1;
             }
         }
         return nums[l] == target ? l : -1;
+    }
+
+    public int searchV4(int[] nums, int target) {
+        int l = 0, r = nums.length; // [l, r)中查找
+        while (l < r) { // l == r 跳出, 也就是[r, r)区间. 如果写l <= r是没有意义的, [r+1, r)没意义
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            // 因为nums[mid]已经判断过了, 所以要去[left, mid) 或 [mid + 1, right)
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                r = mid;
+            }
+        }
+        return -1;
     }
 }
